7a09e22428c61db432c5f7e76dd82b7dcd6c8135
Hyoeunkh <lhe4528@knu.ac.kr>
2024-05-30 10:43:38 +0900

    feat: 초기 화면 세팅

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,11 @@
-import { useEffect, useState } from "react";
-import { Card, CardSkeleton } from "./components/Card";
 import { NavBar } from "./components/NavBar";
+import HomePage from "./pages/HomePage";
 
 export default function App() {
-    const [isPending, setIsPending] = useState(false);
-    const [posts, setPosts] = useState([]);
-
-    useEffect(() => {
-        setIsPending(true);
-        fetch("http://localhost:8080/posts")
-            .then((response) => {
-                return response.json();
-            })
-            .then((posts) => {
-                setPosts(posts);
-            })
-            .finally(() => {
-                setIsPending(false);
-            });
-    }, []);
-
-    return (
-        <>
-            <NavBar />
-
-            {isPending ? (
-                <>
-                    <CardSkeleton />
-                    <CardSkeleton />
-                    <CardSkeleton />
-                    <CardSkeleton />
-                </>
-            ) : (
-                posts.map((post) => {
-                    return <Card key={post.id} title={post.title} author={post.author} likes={post.likes} createdAt={post.createdAt} />;
-                })
-            )}
-        </>
-    );
+  return (
+    <>
+      <NavBar />
+      <HomePage />
+    </>
+  );
 }
